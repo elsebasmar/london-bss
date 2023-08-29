088eb014c5879651225b1944d1b000c84fb3e00c
Catherine Martin <mkpub123@gmail.com>
2023-08-29 12:44:18 +0100

    pushing new version

--- api/weather_data.py
+++ api/weather_data.py
@@ -1,5 +1,6 @@
 # WEATHER DATA FROM OPEN METEO API
 import requests
+import pandas as pd
 
 ## Getting daily sunrise and sunset times from the Open Meteo API
 def get_daily_data(start_date, end_date):
@@ -27,31 +28,48 @@ def get_daily_data(start_date, end_date):
 
 
 ## Getting hourly weather data from the Open Meteo API
-def get_hourly_data(start_date, end_date):
-    url = 'https://archive-api.open-meteo.com/v1/archive'
+#def get_hourly_data(start_date, end_date):
+start_date = "2023-08-20"
+end_date = "2023-08-23"
 
-    params_hourly_dict ={
+url = 'https://archive-api.open-meteo.com/v1/archive'
+
+params_hourly_dict ={
         "latitude":"51.5085", #London latitude - should remain hardcoded
         "longitude":"-0.1780971", #London longitude -should remain hardcoded
         "start_date":start_date, #could be defined in .env and used in the other files
         "end_date":end_date, #could be defined in .env and used in the other files
         "timezone":"Europe/London", #Europe/London - specific to this api
         "hourly":"temperature_2m,precipitation,rain,snowfall,cloudcover,windspeed_10m,winddirection_10m" # specific to this api
-    }
+}
 
-    hourly_weather_response = requests.get(
-        'https://archive-api.open-meteo.com/v1/archive',
-        params=params_hourly_dict).json()
+hourly_weather_response = requests.get(
+    'https://archive-api.open-meteo.com/v1/archive',
+    params=params_hourly_dict).json()
 
-    temperature = hourly_weather_response["hourly"]["temperature_2m"]
-    precipitation = hourly_weather_response["hourly"]["precipitation"]
-    rain = hourly_weather_response["hourly"]["rain"]
-    snow = hourly_weather_response["hourly"]["snowfall"]
-    cloudcover = hourly_weather_response["hourly"]["cloudcover"]
-    windspeed = hourly_weather_response["hourly"]["windspeed_10m"]
-    winddirection= hourly_weather_response["hourly"]["winddirection_10m"]
+timestamp_api = hourly_weather_response["hourly"]["time"]
+temperature_api = hourly_weather_response["hourly"]["temperature_2m"]
+precipitation_api = hourly_weather_response["hourly"]["precipitation"]
+rain_api = hourly_weather_response["hourly"]["rain"]
+snow_api = hourly_weather_response["hourly"]["snowfall"]
+cloudcover_api = hourly_weather_response["hourly"]["cloudcover"]
+windspeed_api = hourly_weather_response["hourly"]["windspeed_10m"]
+winddirection_api = hourly_weather_response["hourly"]["winddirection_10m"]
 
-    print(temperature, precipitation, rain, snow, cloudcover, windspeed, winddirection)
-    #return temperature, precipitation, rain, snow, cloudcover, windspeed, winddirection
+    #print(temperature, precipitation, rain, snow, cloudcover, windspeed, winddirection)
+    #return temperature_api, precipitation_api, rain_api, snow_api, cloudcover_api, windspeed_api, winddirection_api
 
 #get_hourly_data("2023-08-20", "2023-08-21")
+
+weather_data = pd.DataFrame()
+weather_data["timestamp"] = timestamp_api
+weather_data["temperature"] = temperature_api
+weather_data["precipitation"] = precipitation_api
+weather_data["rain"] = rain_api
+weather_data["snow"] = snow_api
+weather_data["cloudcover"] = cloudcover_api
+weather_data["wind_speed"] = windspeed_api
+weather_data["wind_direction"]= winddirection_api
+
+#print(weather_data.head(2))
+#print(weather_data.shape)
