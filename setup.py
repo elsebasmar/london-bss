92b65e4764b7117941e3444b5b1297360993bfc0
Johann Marquez <marquezjohann@gmail.com>
2023-08-29 17:24:06 +0100

    Setup update the version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("requirements.txt") as f:
 requirements = [x.strip() for x in content if "git+" not in x]
 
 setup(name='londonbss',
-    #version="0.0.0",
+    version="0.0.0",
     description="London BSS Model",
     #license="MIT",
     #author="Le Wagon",
