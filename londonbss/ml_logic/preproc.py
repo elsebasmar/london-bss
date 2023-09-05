ca6b2f03a29d92cb2bf7c41816d0f7488545c30b
Catherine Martin <mkpub123@gmail.com>
2023-09-05 15:34:59 +0100

    PREPROCESSING DONE FOR GOOD

--- londonbss/ml_logic/preproc.py
+++ londonbss/ml_logic/preproc.py
@@ -11,7 +11,7 @@ from colorama import Fore, Style
 
 # TRAIN / VAL /TEST split
 X = pd.read_csv("~/code/elsebasmar/london-bss/raw_data/final_features_df.csv")
-final_main_index = []
+
 
 # ENCODE EVENTS
 
@@ -47,11 +47,12 @@ n_lowlimit_lat = 51.523811 # everthing greater than this value is central
 
 ## Encode zone based on events lat and long
 def encode_events_zone(X: pd.DataFrame):
-    # X['event_latitude'].astype("float")
-    # X['event_longitude'].astype("float")
+    X = X.fillna(0)
     london_loc = []
     for i in range(len(X.index)):
-        if X['event_latitude'][i] == "London-wide":
+        if X['event_latitude'][i] == 0:
+            london_loc.append('No_Event')
+        elif X['event_latitude'][i] == "London-wide":
             london_loc.append('London_all') # London wide
         elif float(X['event_latitude'][i]) > c_toplimit_lat and float(X['event_latitude'][i]) < c_lowlimit_lat and float(X['event_longitude'][i])< c_toplimit_lon and float(X['event_longitude'][i])>c_lowlimit_lon:
                 london_loc.append('Central') # Central
@@ -69,12 +70,11 @@ def encode_events_zone(X: pd.DataFrame):
             london_loc.append('Other')
 
     X["London_zone"] = london_loc
-    X.drop(columns=["event_latitude", "event_longitude"], inplace=True)
-    print("london_loc", X.shape)
+    X.drop(columns=["event_latitude", "event_longitude", "event_location", "event_start_date", "event_end_date"], inplace=True)
+
     return X
 
     ## OHE for London zone
-
 def London_zone_manual_ohe(X: pd.DataFrame):
     X["London_zone_Central"] = 0
     X["London_zone_North"] = 0
@@ -83,6 +83,7 @@ def London_zone_manual_ohe(X: pd.DataFrame):
     X["London_zone_South_East"] = 0
     X["London_zone_East"] = 0
     X["London_all"] = 0
+    X["Event"] = 0
     X["London_zone_Central"] = X.apply(lambda x: 1.0 if x['London_zone'] == "Central" else 0.0, axis=1)
     X["London_zone_North"] = X.apply(lambda x: 1.0 if x['London_zone'] == "North" else 0.0, axis=1)
     X["London_zone_West"] = X.apply(lambda x: 1.0 if x['London_zone'] == "West" else 0.0, axis=1)
@@ -90,7 +91,8 @@ def London_zone_manual_ohe(X: pd.DataFrame):
     X["London_zone_South_East"] = X.apply(lambda x: 1.0 if x['London_zone'] == "South_East" else 0.0, axis=1)
     X["London_zone_East"] = X.apply(lambda x: 1.0 if x['London_zone'] == "East" else 0.0, axis=1)
     X["London_all"] = X.apply(lambda x: 1.0 if x['London_zone'] == "London_all" else 0.0, axis=1)
-    print("london_zone_ohe", X.shape)
+    X["Event"] = X.apply(lambda x: 1.0 if x['London_zone'] != "No_Event" else 0.0, axis=1)
+
     return X
 
 def london_all_encoding(X: pd.DataFrame):
@@ -100,8 +102,10 @@ def london_all_encoding(X: pd.DataFrame):
     X["London_zone_South_West"] = X.apply(lambda x: 1.0 if x['London_all'] == 1.0 else x['London_zone_South_West'], axis=1)
     X["London_zone_South_East"] = X.apply(lambda x: 1.0 if x['London_all'] == 1.0 else x['London_zone_South_East'], axis=1)
     X["London_zone_East"] = X.apply(lambda x: 1.0 if x['London_all'] == 1.0 else x['London_zone_East'], axis=1)
-    X.drop(columns= ["London_all"], inplace = True)
-    print("london_all_encoding", X.shape)
+    X.drop(columns= ["London_all", "event_title", "London_zone"], inplace = True)
+
+    X = X.groupby("timestamp").sum()
+
     return X
 
     ## Encode event_title
@@ -112,78 +116,48 @@ def encoding_strings(text):
         text = 1
     return text
 
-X = X.reset_index()
-def encode_event_title(X: pd.DataFrame):
-    X['event_title'] = X['event_title'].fillna(0)
-    X['event_title'] = X['event_title'].apply(encoding_strings)
-    # X.drop(columns = ["event_title"], inplace = True)
-    X = X.groupby("timestamp").sum()
-    print("event_encoding", X.shape)
-    final_main_index = X.index
-    print("initial main index", final_main_index)
-    return X
-
-
-
 events_pipeline = make_pipeline(
     FunctionTransformer(encode_events_zone),
     FunctionTransformer(London_zone_manual_ohe),
     FunctionTransformer(london_all_encoding),
-    FunctionTransformer(encode_event_title),
-)
+    )
 
 # ENCODE OTHER COLUMNS
 
-    ## Encode elisabeth line and lockdown
+    ## Encode elisabeth line and lockdownm strikes and school holidays
 def bool_to_int(X: pd.DataFrame):
-    print('bootoint', X.head())
-    # X.groupby("timestamp").sum()
-    X['elisabeth_line'] = np.where(X['elisabeth_line'] >0, 1, 0)
-    X['lockdown'] = np.where(X['lockdown']>0, 1, 0)
-    # X1 = X1[~X1.index.duplicated(keep="first")
-    # print(X1.shape)
-    print("booltoint", X.shape)
-    print('bootoint', X.head())
-    #print(np.unique(X.index).tolist())
-    return X
 
-    ## Encode strikes and school holidays
-def encode_strikes_holidays(X: pd.DataFrame):
-    print("strikes", X.head())
-    X['strike'] = X['strike'].fillna(0)
-    X['strike'] = X['strike'].apply(encoding_strings)
-    X['school_holidays'] = X['school_holidays'].fillna(0)
-    X['school_holidays'] = X['school_holidays'].apply(encoding_strings)
-    #X = X.drop_duplicates()
-    print("strikes_hols", X.shape)
+    X['elisabeth_line'] = (X['elisabeth_line'] >0).astype(int)
+    X['lockdown'] = (X['lockdown'] >0).astype(int)
+
+    X['strike'] = X['strike'].fillna('NoSTRIKE')
+    X['strike'] = (X['strike'] != 'NoSTRIKE').astype(int)
+
+    X['school_holidays'] = X['school_holidays'].fillna('NoHOL')
+    X['school_holidays'] = (X['school_holidays'] != 'NoHOL').astype(int)
+
+    X['daytime'] = (X['daytime'] == 'daytime').astype(int)
+
+    X = X.groupby("timestamp").sum()
+
     return X
 
     ## Add additional dates details
 
-def encode_day_nighttime(X: pd.DataFrame):
-    #print("daytime", X.head())
-    print("daynighttime", X.shape)
-    X["daytime"] = X["daytime"].replace("daytime", "1")
-    X["daytime"] = X["daytime"].replace("nighttime", "0")
-    #X = X.drop_duplicates()
-    X = X.groupby("timestamp").sum()
-    print("daynighttime", X.shape)
+def turns_into_onetwos(X: pd.DataFrame):
+    for col in X.columns:
+        X[col] = (X[col] > 0).astype(int)
     return X
 
 additional_pipeline = make_pipeline(
         FunctionTransformer(bool_to_int),
-        FunctionTransformer(encode_strikes_holidays),
-        FunctionTransformer(encode_day_nighttime),
+        FunctionTransformer(turns_into_onetwos),
 )
 
 # ENCODE WEATHER COLUMNS
 def weather_drop_duplicates(X: pd.DataFrame):
-    # X_index = pd.DataFrame(index = final_main_index)
-    # print("final X_index", final_main_index)
-    # X2 = X_index.join(X, how = "left")
-    #X = X.drop_duplicates()
     X = X.groupby("timestamp").agg(pd.Series.mode)
-    print("weather", X.shape)
+    # print("weather", X.shape)
     return X
 
 weather_scaler = make_pipeline(FunctionTransformer(weather_drop_duplicates),
@@ -200,6 +174,14 @@ weather_columns = ['temperature', 'rainfall', 'snowfall', 'cloudcover','wind_spe
 events_columns = ['event_title','event_start_date', 'event_end_date', 'event_location','event_latitude', 'event_longitude']
 other_columns = ['elisabeth_line', 'lockdown','school_holidays', 'strike', 'daytime']
 untouched_columns = ['year', 'month', 'day', 'hour', 'weekday']
+
+additional_col_list = ['elisabeth_line', 'lockdown', 'strike', 'school_holidays', 'daytime']
+events_col_list = ['London_zone_Central', 'London_zone_North', 'London_zone_West',
+       'London_zone_South_West', 'London_zone_South_East', 'London_zone_East',
+       'Event']
+final_col_list =  additional_col_list + events_col_list + weather_columns + untouched_columns
+
+print(final_col_list)
 
 final_preprocessor = make_column_transformer(
         (additional_pipeline, other_columns),
@@ -219,12 +201,4 @@ def fit_transform_features(X: pd.DataFrame, stage):
     else:
         print("You didn't provide a stage(train, val or test)")
 
-    return pd.DataFrame(X_processed)
-
-# DROP COLUMNS
-
-
-# put scaling outside of the pipe and fit_transform specifically on X_train only
-# then .transform on X_val and X_test at a later stage
-# create a function if X_train then whatever, if X_val only transform and if X_test only transform --> call the function at a later stage wh
-# when we have the final total DF
+    return pd.DataFrame(X_processed), final_col_list
